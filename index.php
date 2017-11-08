a6860f2c174aec278eef1f971a8e643f8c31b612
ewclide <ewclide@yandex.ru>
2017-11-08 17:19:12 +0300

    add files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+	<link rel="stylesheet" href="layoutbox.css">
+</head>
+<style>
+	.asd {
+	    width: 300px;
+	    height: 300px;
+	    margin: 10px;
+	    display: inline-block;
+	    background: yellow;
+	}
+</style>
+<body style="height: 1000px">
+	<div class="--col --x-mid --y-full _full">
+		<div style="background: blue"><div class="asd"></div></div>
+		<div style="background: red"><div class="asd"></div></div>
+	</div>
+</body>
+</html>
