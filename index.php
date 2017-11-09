95eeb4e58fb9fab89a228d4902615f68cd143602
ewclide <ewclide@yandex.ru>
2017-11-09 23:59:14 +0300

    add row-3 and row-4

--- index.php
+++ index.php
@@ -4,24 +4,197 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Document</title>
-	<style>
-		.asd {
-		    width: 300px;
-		    height: 300px;
-		    margin: 10px;
-		    display: inline-block;
-		    background: yellow;
-		}
-	</style>
+	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="layoutbox.css">
 </head>
-<body style="height: 1000px">
-	<div class="_content">
-		<div class="--row --x-left">
-			<div class="--rubb" style="background: black; width: 400px; height: 200px">test</div>
-			<div style="background: red" >Test</div>
-			<div style="background: blue" >Test</div>
+<body class="--col">
+	<div class="head">
+		<div class="_content --row --x-full">
+			<div class="logo">
+				<div class="logos"></div>
+			</div>
+			<div class="address --col --y-mid">
+				<p class="_icon-left">City Some, Street Some, house 1</p>
+			</div>
+			<div class="phone --col --x-mid --y-mid">
+				<p class="_icon-left">+7 (111) 222-33-44</p>
+				<a href="">recall</a>
+			</div>
+		</div>
+		<div class="menu">
+			<div class="_content">
+				<ul class="--row _trans-250-full">
+					<li><a href="">Category 1</a></li>
+					<li><a href="">Category 2</a></li>
+					<li><a href="">Category 3</a></li>
+					<li><a href="">Category 4</a></li>
+					<li><a href="">Category 5</a></li>
+					<li><a href="">Category 6</a></li>
+					<li><a href="">Category 7</a></li>
+				</ul>
+			</div>
 		</div>
 	</div>
+	<div class="wrapper">
+		<div class="_content">
+			<div class="prod-list --row-3 --x-left">
+				<div class="prod --col rubb">
+
+					<div class="prod-item">
+						<div class="image"></div>
+						<div class="info --col">
+							<h2>Some product</h2>
+							<div class="price --row --nowrap --x-full">
+								<p class="oldprice">12 000 rub</p>
+								<p class="newprice">10 000 rub</p>
+							</div>
+							<div>
+								<a href="">more</a>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<div class="prod --col --rubb">
+					<div class="image"></div>
+					<div class="info --col">
+						<h2>Some product</h2>
+						<div class="price --row --nowrap --x-full">
+							<p class="oldprice">12 000 rub</p>
+							<p class="newprice">10 000 rub</p>
+						</div>
+						<div>
+							<a href="">more</a>
+						</div>
+					</div>
+				</div>
+				<div class="prod --col --rubb">
+					<div class="image"></div>
+					<div class="info --col">
+						<h2>Some product</h2>
+						<div class="price --row --nowrap --x-full">
+							<p class="oldprice">12 000 rub</p>
+							<p class="newprice">10 000 rub</p>
+						</div>
+						<div>
+							<a href="">more</a>
+						</div>
+					</div>
+				</div>
+
+								<div class="prod --col --rubb">
+					<div class="image"></div>
+					<div class="info --col">
+						<h2>Some product</h2>
+						<div class="price --row --nowrap --x-full">
+							<p class="oldprice">12 000 rub</p>
+							<p class="newprice">10 000 rub</p>
+						</div>
+						<div>
+							<a href="">more</a>
+						</div>
+					</div>
+				</div>
+
+								<div class="prod --col --rubb">
+					<div class="image"></div>
+					<div class="info --col">
+						<h2>Some product</h2>
+						<div class="price --row --nowrap --x-full">
+							<p class="oldprice">12 000 rub</p>
+							<p class="newprice">10 000 rub</p>
+						</div>
+						<div>
+							<a href="">more</a>
+						</div>
+					</div>
+				</div>
+
+								<div class="prod --col --rubb">
+					<div class="image"></div>
+					<div class="info --col">
+						<h2>Some product</h2>
+						<div class="price --row --nowrap --x-full">
+							<p class="oldprice">12 000 rub</p>
+							<p class="newprice">10 000 rub</p>
+						</div>
+						<div>
+							<a href="">more</a>
+						</div>
+					</div>
+				</div>
+
+								<div class="prod --col --rubb">
+					<div class="image"></div>
+					<div class="info --col">
+						<h2>Some product</h2>
+						<div class="price --row --nowrap --x-full">
+							<p class="oldprice">12 000 rub</p>
+							<p class="newprice">10 000 rub</p>
+						</div>
+						<div>
+							<a href="">more</a>
+						</div>
+					</div>
+				</div>
+
+								<div class="prod --col --rubb">
+					<div class="image"></div>
+					<div class="info --col">
+						<h2>Some product</h2>
+						<div class="price --row --nowrap --x-full">
+							<p class="oldprice">12 000 rub</p>
+							<p class="newprice">10 000 rub</p>
+						</div>
+						<div>
+							<a href="">more</a>
+						</div>
+					</div>
+				</div>
+
+								<div class="prod --col --rubb">
+					<div class="image"></div>
+					<div class="info --col">
+						<h2>Some product</h2>
+						<div class="price --row --nowrap --x-full">
+							<p class="oldprice">12 000 rub</p>
+							<p class="newprice">10 000 rub</p>
+						</div>
+						<div>
+							<a href="">more</a>
+						</div>
+					</div>
+				</div>
+				<div class="prod --col --rubb">
+					<div class="image"></div>
+					<div class="info --col">
+						<h2>Some product</h2>
+						<div class="price --row --nowrap --x-full">
+							<p class="oldprice">12 000 rub</p>
+							<p class="newprice">10 000 rub</p>
+						</div>
+						<div>
+							<a href="">more</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="prod --col --rubb">
+					<div class="image"></div>
+					<div class="info --col">
+						<h2>Some product</h2>
+						<div class="price --row --nowrap --x-full">
+							<p class="oldprice">12 000 rub</p>
+							<p class="newprice">10 000 rub</p>
+						</div>
+						<div>
+							<a href="">more</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer"></div>
 </body>
 </html>
