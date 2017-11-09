040486d4d1eff465dc75835964eaadb4b9706905
ewclide <ewclide@yandex.ru>
2017-11-09 15:28:05 +0300

    update

--- index.php
+++ index.php
@@ -4,21 +4,23 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Document</title>
+	<style>
+		.asd {
+		    width: 300px;
+		    height: 300px;
+		    margin: 10px;
+		    display: inline-block;
+		    background: yellow;
+		}
+	</style>
 	<link rel="stylesheet" href="layoutbox.css">
 </head>
-<style>
-	.asd {
-	    width: 300px;
-	    height: 300px;
-	    margin: 10px;
-	    display: inline-block;
-	    background: yellow;
-	}
-</style>
 <body style="height: 1000px">
-	<div class="--col --x-mid --y-full _full">
-		<div style="background: blue"><div class="asd"></div></div>
-		<div style="background: red"><div class="asd"></div></div>
+	<div class="_content">
+		<div class="--row">
+			<div style="background: red">Test</div>
+			<div style="background: blue">Test</div>
+		</div>
 	</div>
 </body>
 </html>
