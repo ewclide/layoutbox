4f2667188afb2d57f257aa920ed6670876c86f93
ewclide <ewclide@yandex.ru>
2017-11-10 20:55:37 +0300

    remove x y align by left right top bot mid align

--- index.php
+++ index.php
@@ -7,194 +7,21 @@
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="layoutbox.css">
 </head>
-<body class="--col">
-	<div class="head">
-		<div class="_content --row --x-full">
-			<div class="logo">
-				<div class="logos"></div>
-			</div>
-			<div class="address --col --y-mid">
-				<p class="_icon-left">City Some, Street Some, house 1</p>
-			</div>
-			<div class="phone --col --x-mid --y-mid">
-				<p class="_icon-left">+7 (111) 222-33-44</p>
-				<a href="">recall</a>
-			</div>
-		</div>
-		<div class="menu">
-			<div class="_content">
-				<ul class="--row _trans-250-full">
-					<li><a href="">Category 1</a></li>
-					<li><a href="">Category 2</a></li>
-					<li><a href="">Category 3</a></li>
-					<li><a href="">Category 4</a></li>
-					<li><a href="">Category 5</a></li>
-					<li><a href="">Category 6</a></li>
-					<li><a href="">Category 7</a></li>
-				</ul>
-			</div>
+<body style="height: 700px">
+	<div class="_cont">
+		<div class="--tile-4">
+			<div class="asd"></div>
+			<div class="asd"></div>
+			<div class="asd"></div>
+			<div class="asd"></div>
+			<div class="asd"></div>
+			<div class="asd"></div>
+			<div class="asd"></div>
+			<div class="asd"></div>
+			<div class="asd"></div>
+			<div class="asd"></div>
+			<div class="asd"></div>
 		</div>
 	</div>
-	<div class="wrapper">
-		<div class="_content">
-			<div class="prod-list --tile-4">
-				<div class="prod --col">
-
-					<div class="prod-item">
-						<div class="image"></div>
-						<div class="info --col">
-							<h2>Some product</h2>
-							<div class="price --row --nowrap --x-full">
-								<p class="oldprice">12 000 rub</p>
-								<p class="newprice">10 000 rub</p>
-							</div>
-							<div>
-								<a href="">more</a>
-							</div>
-						</div>
-					</div>
-
-				</div>
-				<div class="prod --col">
-					<div class="image"></div>
-					<div class="info --col">
-						<h2>Some product</h2>
-						<div class="price --row --nowrap --x-full">
-							<p class="oldprice">12 000 rub</p>
-							<p class="newprice">10 000 rub</p>
-						</div>
-						<div>
-							<a href="">more</a>
-						</div>
-					</div>
-				</div>
-				<div class="prod --col">
-					<div class="image"></div>
-					<div class="info --col">
-						<h2>Some product</h2>
-						<div class="price --row --nowrap --x-full">
-							<p class="oldprice">12 000 rub</p>
-							<p class="newprice">10 000 rub</p>
-						</div>
-						<div>
-							<a href="">more</a>
-						</div>
-					</div>
-				</div>
-
-								<div class="prod --col">
-					<div class="image"></div>
-					<div class="info --col">
-						<h2>Some product</h2>
-						<div class="price --row --nowrap --x-full">
-							<p class="oldprice">12 000 rub</p>
-							<p class="newprice">10 000 rub</p>
-						</div>
-						<div>
-							<a href="">more</a>
-						</div>
-					</div>
-				</div>
-
-								<div class="prod --col">
-					<div class="image"></div>
-					<div class="info --col">
-						<h2>Some product</h2>
-						<div class="price --row --nowrap --x-full">
-							<p class="oldprice">12 000 rub</p>
-							<p class="newprice">10 000 rub</p>
-						</div>
-						<div>
-							<a href="">more</a>
-						</div>
-					</div>
-				</div>
-
-								<div class="prod --col">
-					<div class="image"></div>
-					<div class="info --col">
-						<h2>Some product</h2>
-						<div class="price --row --nowrap --x-full">
-							<p class="oldprice">12 000 rub</p>
-							<p class="newprice">10 000 rub</p>
-						</div>
-						<div>
-							<a href="">more</a>
-						</div>
-					</div>
-				</div>
-
-								<div class="prod --col">
-					<div class="image"></div>
-					<div class="info --col">
-						<h2>Some product</h2>
-						<div class="price --row --nowrap --x-full">
-							<p class="oldprice">12 000 rub</p>
-							<p class="newprice">10 000 rub</p>
-						</div>
-						<div>
-							<a href="">more</a>
-						</div>
-					</div>
-				</div>
-
-								<div class="prod --col">
-					<div class="image"></div>
-					<div class="info --col">
-						<h2>Some product</h2>
-						<div class="price --row --nowrap --x-full">
-							<p class="oldprice">12 000 rub</p>
-							<p class="newprice">10 000 rub</p>
-						</div>
-						<div>
-							<a href="">more</a>
-						</div>
-					</div>
-				</div>
-
-								<div class="prod --col">
-					<div class="image"></div>
-					<div class="info --col">
-						<h2>Some product</h2>
-						<div class="price --row --nowrap --x-full">
-							<p class="oldprice">12 000 rub</p>
-							<p class="newprice">10 000 rub</p>
-						</div>
-						<div>
-							<a href="">more</a>
-						</div>
-					</div>
-				</div>
-				<div class="prod --col">
-					<div class="image"></div>
-					<div class="info --col">
-						<h2>Some product</h2>
-						<div class="price --row --nowrap --x-full">
-							<p class="oldprice">12 000 rub</p>
-							<p class="newprice">10 000 rub</p>
-						</div>
-						<div>
-							<a href="">more</a>
-						</div>
-					</div>
-				</div>
-
-				<div class="prod --col">
-					<div class="image"></div>
-					<div class="info --col">
-						<h2>Some product</h2>
-						<div class="price --row --nowrap --x-full">
-							<p class="oldprice">12 000 rub</p>
-							<p class="newprice">10 000 rub</p>
-						</div>
-						<div>
-							<a href="">more</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="footer"></div>
 </body>
 </html>
